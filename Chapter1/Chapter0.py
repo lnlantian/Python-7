5e02c8f54dd3c16339998b6262afd983eed1166d
Lantianor <lantianor@126.com>
2017-02-21 18:22:53 +0800

    Update Chapter0.py

--- Chapter1/Chapter0.py
+++ Chapter1/Chapter0.py
@@ -3,33 +3,19 @@
 
 print (3, -1, 3.14159, -2.8)
 
-
 print (type(3))
 print (type(3.14159))
 print (type(3.0))
 
-
-
 print (int(3.14159), int(-2.8))
 print (float(3), float(-1))
-
-
 
 print (3.1415926535897932384626433832795028841971)
 print (1.4142135623730950488016887242096980785696)
 
-# arithmetic operators
-# +		plus		addition
-# -		minus		subtraction
-# *		times		multiplication
-# /		divided by 	division
-# **    power		exponentiation
-
 print (1 + 2, 3 - 4, 5 * 6, 2 ** 5)
 
-
 print (1.0 / 3, 5.0 / 2.0, -7 / 3.0)
-
 
 print (1 / 3, 5 / 2, -7 / 3)
 
@@ -48,8 +34,6 @@ print (2 + 12)
 
 print (1 * (2 + 3) * 4)
 print (1 * 5 * 4)
-
-
 
 # valid variable names - consists of letters, numbers, underscore (_)
 # starts with letter or underscore
